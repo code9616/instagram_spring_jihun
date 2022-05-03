a0216b019a4e06c7ef9e11b353572f1706085ea1
ITPS <ITPS@gmail.com>
2022-05-04 01:58:30 +0900

    인스타 회원가입 과제

--- instagram_spring_jihun/src/main/java/com/instagram/jihun/web/controller/AuthController.java
+++ instagram_spring_jihun/src/main/java/com/instagram/jihun/web/controller/AuthController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.instagram.jihun.web.dto.auth.SignupRequestDto;
 
 @Controller
 public class AuthController {
@@ -13,4 +16,18 @@ public class AuthController {
 		
 		return "auth/signup";
 	}
+	@ResponseBody
+	@RequestMapping(value = "/auth/signup", method = RequestMethod.POST)
+	public String signupSubmit(SignupRequestDto signupRequestDto) {
+		boolean result = authService.signup(signupRequestDto);
+		
+		return null;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/auth/username/check", method = RequestMethod.POST)
+	public String usernameCheck(String username) {
+		return Boolean.toString(authService.checkUsername(username));
+	}
+	
 }
